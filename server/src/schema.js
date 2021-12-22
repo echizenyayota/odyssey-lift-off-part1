20b500a0f7229e650ff23cb79c8b43d3ab428360
echizenyayota <virtualmisso30@gmail.com>
2021-12-22 15:34:51 +0900

    The Query type

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,6 +2,10 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   "A track is a group of Modules that teaches about a specific topic"
+  type Query {
+    tracksForHome: [Track!]!
+  }
+
   type Track {
     id: ID!
     title: String!
