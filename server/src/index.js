d5e0d24a2e142eb83555dd0de43fe0ad6fe8daaf
echizenyayota <virtualmisso30@gmail.com>
2021-12-23 15:27:40 +0900

    Back-end first steps (fixed_2)

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ const mocks = {
         photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
       };
     },
-    thumbnal: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
     modulesCount: () => 6
   })
