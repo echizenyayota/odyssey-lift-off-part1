2e4dd3ff87b58c943241cc16d278334c7f21bc15
echizenyayota <virtualmisso30@gmail.com>
2021-12-24 14:16:07 +0900

    A query in a component

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 
 /**
  * Tracks Page is the Catstronauts home page.
